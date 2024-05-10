b6f48f08977a7331a768215585fa91594bc86929
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-10 23:52:48 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+//       int a = 10;
+//       int x=10 , y=20;
+//       float b = 12.55f;
+//       char c = 'A';
+//       String str = "hello";
+//        System.out.println("number is "+y);
+
+
+//        int a = 10 , b = 20;
+//        System.out.println("sum of "+a+" and "+b+" is " +(a+b));
+
+
+        
+
+
+
+
+    }
+}
